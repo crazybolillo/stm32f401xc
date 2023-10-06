3c44c8b7f0d0268de6917ab57b88f581e5d7aba1
CrazyBolillo <antonio@zoftko.com>
2023-10-06 13:04:57 -0600

    ref: create common.c for common procedures
    Procedures like setting up the clock tree will be repeated across
    programs so it makes sense to place it under a common file.

--- src/blink.c
+++ src/blink.c
@@ -1,34 +1,13 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-const uint8_t MCO1PRE_DIV4 = 0x06;
-const uint8_t MCO1_PPL = 0x03;
-const uint8_t PPRE1_DIV2 = 0x04;
+#include "common.h"
 
 static TaskHandle_t blinkTaskHandle;
 
 void setupHardware(void) {
     __disable_irq();
-    FLASH->ACR |= FLASH_ACR_LATENCY_2WS;
-    while ((FLASH->ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_2WS) {}
-
-    RCC->CFGR |=
-        (MCO1PRE_DIV4 << RCC_CFGR_MCO1PRE_Pos) | (MCO1_PPL << RCC_CFGR_MCO1_Pos) | (PPRE1_DIV2 << RCC_CFGR_PPRE1_Pos);
-
-    RCC->CR |= RCC_CR_HSEON;
-    while ((RCC->CR & RCC_CR_HSERDY) == 0) {}
-
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-    RCC->PLLCFGR = (6 << RCC_PLLCFGR_PLLQ_Pos) | (288 << RCC_PLLCFGR_PLLN_Pos) | (25 << RCC_PLLCFGR_PLLM_Pos) |
-                   (1 << RCC_PLLCFGR_PLLP_Pos) | RCC_PLLCFGR_PLLSRC_HSE;
-    RCC->CR |= RCC_CR_PLLON;
-    while ((RCC->CR & RCC_CR_PLLRDY) == 0) {}
-
-    RCC->CFGR |= RCC_CFGR_SW_PLL;
-    while ((RCC->CFGR & RCC_CFGR_SWS) == 0) {}
-
-    SystemCoreClockUpdate();
-    SysTick_Config(SystemCoreClock / configTICK_RATE_HZ);
+    setup_clock();
 
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
     GPIOC->ODR = 0;
---
+++ src/common.c
@@ -0,0 +1,31 @@
+#include "common.h"
+
+#include <FreeRTOSConfig.h>
+#include <stm32f4xx.h>
+
+static const uint8_t MCO1PRE_DIV4 = 0x06;
+static const uint8_t MCO1_PPL = 0x03;
+static const uint8_t PPRE1_DIV2 = 0x04;
+
+void setup_clock(void) {
+    FLASH->ACR |= FLASH_ACR_LATENCY_2WS;
+    while ((FLASH->ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_2WS) {}
+
+    RCC->CFGR |=
+        (MCO1PRE_DIV4 << RCC_CFGR_MCO1PRE_Pos) | (MCO1_PPL << RCC_CFGR_MCO1_Pos) | (PPRE1_DIV2 << RCC_CFGR_PPRE1_Pos);
+
+    RCC->CR |= RCC_CR_HSEON;
+    while ((RCC->CR & RCC_CR_HSERDY) == 0) {}
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    RCC->PLLCFGR = (6 << RCC_PLLCFGR_PLLQ_Pos) | (288 << RCC_PLLCFGR_PLLN_Pos) | (25 << RCC_PLLCFGR_PLLM_Pos) |
+                   (1 << RCC_PLLCFGR_PLLP_Pos) | RCC_PLLCFGR_PLLSRC_HSE;
+    RCC->CR |= RCC_CR_PLLON;
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0) {}
+
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while ((RCC->CFGR & RCC_CFGR_SWS) == 0) {}
+
+    SystemCoreClockUpdate();
+    SysTick_Config(SystemCoreClock / configTICK_RATE_HZ);
+}
