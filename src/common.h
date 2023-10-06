a08db474b3a3d6b663b3a6d8e22507c381512162
CrazyBolillo <antonio@zoftko.com>
2023-10-06 13:04:57 -0600

    ref: create common.c for common procedures
    Procedures like setting up the clock tree will be repeated across
    programs so it makes sense to place it under a common file.

---
+++ src/common.h
@@ -0,0 +1,16 @@
+#ifndef STM32F4_COMMON_H
+#define STM32F4_COMMON_H
+
+#include <stdint.h>
+
+enum GPIO_MODER { MODER_INPUT = 0x00, MODER_OUTPUT = 0x01, MODER_ALTERNATE = 0x02, MODER_ANALOG = 0x03 };
+
+/**
+ * Setup the clock tree for USB operation and increase CPU frequency. The internal PLL is used to achieve this.
+ * CPU clock set to 72 MHz
+ * USB clock set to 48 MHz
+ * USB clock divided by 4 (12 MHz) output on MCO1.
+ */
+void setup_clock(void);
+
+#endif  // STM32F4_COMMON_H
